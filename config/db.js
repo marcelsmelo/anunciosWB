ce672b4fe6b058aad66dfff757b3bdba60b49ecf
Marcel Melo <marcel.msmelo@gmail.com>
2021-03-16 14:15:00 -0300

    Add Remote Database

--- config/db.js
+++ config/db.js
@@ -2,29 +2,19 @@ module.exports = {
     /**
      * Local
      */
-    host: 'localhost',
-    port: 3306,
-    database: 'anunciosWB',
-    username: 'root',
-    password: ''
+    // host: 'localhost',
+    // port: 3306,
+    // database: 'anunciosWB',
+    // username: 'root',
+    // password: ''
 
     /**
      * Desenvolvimento
      */
-    // host: 'mysql669.umbler.com',
-    // port: 41890,
-    // database: 'appdoacao',
-    // username: 'msmelo',
-    // password: '4*8|[xmBwbH'
+    host: 'mysql743.umbler.com',
+    port: 41890,
+    database: 'anuncios_db',
+    username: 'marcelmelo',
+    password: '_x4yC55KU|'
 
-
-    /**
-     * Production
-     */
-    // host: 'mysql669.umbler.com',
-    // port: 41890,
-    // database: 'doebrasil',
-    // username: 'doebrasil-admin',
-    // password: 'pDB6hkh4nDf2NW3'
-    
 }
